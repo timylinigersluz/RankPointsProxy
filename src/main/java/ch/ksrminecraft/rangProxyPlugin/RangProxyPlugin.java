2b544ba2f46d893744f51dbd2c33eca4b1a772e8
Samuel Huwiler <Samuel_huwiler@sluz.ch>
2025-03-17 16:59:08 +0100

    INIT

---
+++ src/main/java/ch/ksrminecraft/rangProxyPlugin/RangProxyPlugin.java
@@ -0,0 +1,18 @@
+package ch.ksrminecraft.rangProxyPlugin;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(id = "rangproxyplugin", name = "RangProxyPlugin", version = "1.0")
+public class RangProxyPlugin {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
