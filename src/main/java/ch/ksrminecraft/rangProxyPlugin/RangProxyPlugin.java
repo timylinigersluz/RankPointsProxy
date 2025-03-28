5672bc0505087bf13aa2738b37fa6cb2151a0dfc
Samuel Huwiler <Samuel_huwiler@sluz.ch>
2025-03-28 12:06:22 +0100

    Finaly some progress, while JEAAAAAAAN is driving his toys in a circle

--- src/main/java/ch/ksrminecraft/rangProxyPlugin/RangProxyPlugin.java
+++ src/main/java/ch/ksrminecraft/rangProxyPlugin/RangProxyPlugin.java
@@ -1,6 +1,7 @@
 package ch.ksrminecraft.rangProxyPlugin;
 
 import com.google.inject.Inject;
+import com.google.inject.Injector;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
@@ -8,6 +9,8 @@ import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import org.slf4j.Logger;
 import ch.ksrminecraft.RangAPI.RangAPI;
 import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.serialize.SerializationException;
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 
 import java.io.IOException;
@@ -16,20 +19,21 @@ import java.nio.file.Paths;
 
 @Plugin(id = "rangproxyplugin", name = "RangProxyPlugin", version = "1.0")
 public class RangProxyPlugin {
+    private final Path dataDirectory = null;
 
     @Inject
     private Logger logger;
 
-    @Subscribe
-    public void onProxyInitialization(ProxyInitializeEvent eventD, @DataDirectory Path dataDirectory) {
+    @Inject
+    public RangProxyPlugin(@DataDirectory Path dataDirectory) {
         // https://github.com/SpongePowered/Configurate/wiki/Getting-Started
         //TODO make dir if not there
         //TODO make conf if not there
-        Path configpath = Paths.get("resources");
-        final YamlConfigurationLoader loader = YamlConfigurationLoader.builder().path(dataDirectory.resolve(dataDirectory.resolve(configpath))).build();
-        CommentedConfigurationNode root;
+        Path configpath = Paths.get("resources.yaml");
+        final YamlConfigurationLoader loader = YamlConfigurationLoader.builder().path(dataDirectory.resolve(configpath)).build();
+        CommentedConfigurationNode root = null;
         try {
-        root = loader.load();
+            root = loader.load();
         } catch (IOException e) {
             System.err.println("An error occurred while loading this configuration: " + e.getMessage());
             if (e.getCause() != null) {
@@ -37,8 +41,22 @@ public class RangProxyPlugin {
             }
             System.exit(1);
         }
+        try {
+            root.node("test").set(10);
+        } catch (SerializationException e) {
+            System.out.println("An error occurred while setting up the configuration: " + e.getMessage());
+        }
+        // And save the node back to the file
+        try {
+            loader.save(root);
+        } catch (final ConfigurateException e) {
+            System.err.println("Unable to save your messages configuration! Sorry! " + e.getMessage());
+            System.exit(1);
+        }
+    }
 
-
-        RangAPI api = new RangAPI("asf", "sa", "asdf");
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent eventD){
+        //RangAPI api = new RangAPI("asf", "sa", "asdf");
     }
 }
