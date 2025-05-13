fb0261d9466573e7f98c12de3bc9895b752f463e
Nanoboter <Jean_Brawler@outlook.com>
2025-05-13 12:29:58 +0200

    removed unnecessary imports

--- src/main/java/ch/ksrminecraft/rangProxyPlugin/RangProxyPlugin.java
+++ src/main/java/ch/ksrminecraft/rangProxyPlugin/RangProxyPlugin.java
@@ -3,14 +3,10 @@ package ch.ksrminecraft.rangProxyPlugin;
 import ch.ksrminecraft.rangProxyPlugin.commands.AddPointsCommand;
 import ch.ksrminecraft.rangProxyPlugin.commands.SetPointsCommand;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
-import net.luckperms.api.LuckPerms;
-import net.luckperms.api.LuckPermsProvider;
-import net.luckperms.api.model.user.User;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import org.slf4j.Logger;
 import ch.ksrminecraft.RangAPI.RangAPI;
@@ -23,7 +19,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.util.UUID;
 
 @Plugin(id = "rangproxyplugin", name = "RangProxyPlugin", version = "1.0")
 public class RangProxyPlugin {
